8ac6eb7ad895f7972ac89944e85e5ee5580b3e82
Vladyslav Khomenko <vladislav.homenko@gmail.com>
2025-02-21 15:20:03 +0200

    feat: Add initial implementation of platform_maps_flutter_google_web plugin

---
+++ packages/platform_maps_flutter_google_web/test/platform_maps_flutter_google_web_test.dart
@@ -0,0 +1,57 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:mockito/mockito.dart';
+import 'package:platform_maps_flutter_google_web/platform_maps_flutter_google_web.dart';
+import 'package:platform_maps_flutter_google_web/src/google_maps_bitmap_descriptor.dart';
+import 'package:platform_maps_flutter_google_web/src/google_maps_camera_update.dart';
+import 'package:platform_maps_flutter_google_web/src/google_maps_widget.dart';
+import 'package:platform_maps_flutter_platform_interface/platform_maps_flutter_platform_interface.dart';
+
+// Mock dependencies
+class MockPlatformMapsPlatformWidgetCreationParams extends Mock
+    implements PlatformMapsPlatformWidgetCreationParams {}
+
+void main() {
+  group('PlatformMapsGoogleWeb Tests', () {
+    late PlatformMapsGoogleWeb platformMapsGoogleWeb;
+
+    setUp(() {
+      platformMapsGoogleWeb = PlatformMapsGoogleWeb();
+    });
+
+    test('should register itself as the instance', () {
+      // Act
+      PlatformMapsGoogleWeb.registerWith(Registrar());
+
+      // Assert
+      expect(PlatformMapsPlatform.instance, isA<PlatformMapsGoogleWeb>());
+    });
+
+    test('should create a GoogleMapsWidget when calling createPlatformMapsPlatformWidget', () {
+      // Arrange
+      final mockParams = MockPlatformMapsPlatformWidgetCreationParams();
+
+      // Act
+      final widget = platformMapsGoogleWeb.createPlatformMapsPlatformWidget(mockParams);
+
+      // Assert
+      expect(widget, isA<GoogleMapsWidget>());
+    });
+
+    test('should create a GoogleMapsPlatformBitmapDescriptor when calling createBitmapDescriptor', () {
+      // Act
+      final bitmapDescriptor = platformMapsGoogleWeb.createBitmapDescriptor();
+
+      // Assert
+      expect(bitmapDescriptor, isA<GoogleMapsPlatformBitmapDescriptor>());
+    });
+
+    test('should create a GoogleMapsPlatformCameraUpdate when calling createPlatformCameraUpdate', () {
+      // Act
+      final cameraUpdate = platformMapsGoogleWeb.createPlatformCameraUpdate();
+
+      // Assert
+      expect(cameraUpdate, isA<GoogleMapsPlatformCameraUpdate>());
+    });
+  });
+}
