6cf8a6af8bd92a1961a9d981b98969de91c11318
Vladyslav Khomenko <vladislav.homenko@gmail.com>
2025-10-07 16:04:02 +0300

    refactor: Simplify constructor syntax for MyApp and MyHomePage, update mapId reference in configuration

--- packages/platform_maps_flutter/example/lib/main.dart
+++ packages/platform_maps_flutter/example/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:platform_maps_flutter/platform_maps_flutter.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
